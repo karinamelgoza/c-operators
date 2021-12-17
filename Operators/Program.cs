36a4bf6a2763d59c84cd3b58042af7ef5ec3f777
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-17 13:13:06 -0700

    initial commit

---
+++ Operators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+        }
+    }
+}
