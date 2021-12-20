69d4d1bbaada45ba20e65d72827516df0d20d60c
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-20 12:48:32 -0700

    Adds practice with conditional operators

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -16,6 +16,56 @@ namespace Operators
 
             bool isSunny = true;
             Console.WriteLine($"is it sunny? {!isSunny}");
+
+            //increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine($"num is {num}");
+            Console.WriteLine($"num is {num++}");
+            //pre increment
+            Console.WriteLine($"num is {++num}");
+
+            //decrement operator
+            num--;
+            Console.WriteLine($"num is {num}");
+            Console.WriteLine($"num is {num--}");
+            //pre decrement
+            Console.WriteLine($"num is {--num}");
+
+            int result;
+            result = num1 + num2;
+            Console.WriteLine($"result of num1 + num2 is {result}");
+            result = num1 - num2;
+            Console.WriteLine($"result of num1 - num2 is {result}");
+            result = num1 / num2;
+            Console.WriteLine($"result of num1 / num2 is {result}");
+            result = num1 * num2;
+            Console.WriteLine($"result of num1 * num2 is {result}");
+            result = num1 % num2;
+            Console.WriteLine($"result of num1 % num2 is {result}");
+
+            //relational and type operators
+            bool isLower;
+            isLower = num1 < num2;
+            Console.WriteLine($"result of num1 < num2 is {isLower}");
+
+            //equality operator
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine($"result of num1 == num2 is {isEqual}");
+
+            isEqual = num1 != num2;
+            Console.WriteLine($"result of num1 != num2 is {isEqual}");
+
+            //conditional operators
+            bool isLowerAndSunny;
+            //condition1 AND condition2
+            isLowerAndSunny = isLower && isSunny;
+            Console.WriteLine($"result of isLower && isSunny is {isLowerAndSunny}");
+
+            //condition1 OR condition2 
+            isLowerAndSunny = isLower || isSunny;
+            Console.WriteLine($"result of isLower || isSunny is {isLowerAndSunny}");
         }
     }
 }
